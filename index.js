c73ad2c787ac4b863d49c0be626e2e9f4e326a19
ChrisAruiza1015 <chrisaruiza57@gmail.com>
2022-01-10 13:26:06 -0800

    Working on those last tasls

--- index.js
+++ index.js
@@ -31,9 +31,28 @@ const users = {
           id: 'zap555', 
           name: 'Dennis',
           job: 'Bartender',
-       }
+       },
+       {
+        "id": "qwe123",
+        "job": "Zookeeper",
+        "name": "Cindy"
+        }
     ]
  }
+
+app.get('/users/', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+
  app.get('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
     let result = findUserById(id);
@@ -48,6 +67,16 @@ const users = {
 function findUserById(id) {
     return users['users_list'].find( (user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
 }
 
 const findUserByName = (name) => { 
